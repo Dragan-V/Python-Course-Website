1800016aa3e284be4cd02833cab029e9aea31105
LifesGoood <dragan.velinov10@gmail.com>
2023-06-03 18:56:40 +0200

    L/R buttons
    added next previous buttons for the zadachi tab

--- js/zadachi.js
+++ js/zadachi.js
@@ -7,9 +7,9 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
     carousel.insertAdjacentHTML(
         "beforeend",
         `
-          <div class="carousel__nav">
-              ${buttonsHtml.join("")}
-          </div>
+      <div class="carousel__nav">
+          ${buttonsHtml.join("")}
+      </div>
       `
     );
 
@@ -30,7 +30,69 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
         });
     });
 
-    // Select the first item on page load
+    // Select the second item on page load
     items[0].classList.add("carousel__item--selected");
     buttons[0].classList.add("carousel__button--selected");
+
+    const prevButton = document.createElement("span");
+    prevButton.classList.add("carousel__button", "carousel__button--prev");
+    prevButton.addEventListener("click", () => {
+        const selectedItem = carousel.querySelector(".carousel__item--selected");
+        const prevItem = selectedItem.previousElementSibling;
+
+        if (prevItem) {
+            selectedItem.classList.remove("carousel__item--selected");
+            prevItem.classList.add("carousel__item--selected");
+
+            const selectedIndex = Array.from(items).indexOf(prevItem);
+
+            buttons.forEach((button) =>
+                button.classList.remove("carousel__button--selected")
+            );
+            buttons[selectedIndex].classList.add("carousel__button--selected");
+        } else {
+            // Reached the first item, sends you to the last
+            selectedItem.classList.remove("carousel__item--selected");
+            items[items.length - 1].classList.add("carousel__item--selected");
+
+            buttons.forEach((button) =>
+                button.classList.remove("carousel__button--selected")
+            );
+            buttons[items.length - 1].classList.add("carousel__button--selected");
+        }
+    });
+    carousel.querySelector(".carousel__nav").prepend(prevButton);
+
+    const nextButton = document.createElement("span");
+    nextButton.classList.add("carousel__button", "carousel__button--next");
+    nextButton.addEventListener("click", () => {
+        const selectedItem = carousel.querySelector(".carousel__item--selected");
+        const nextItem = selectedItem.nextElementSibling;
+
+        if (
+            nextItem &&
+            !nextItem.classList.contains("carousel__button--next") &&
+            !nextItem.classList.contains("carousel__nav")
+        ) {
+            selectedItem.classList.remove("carousel__item--selected");
+            nextItem.classList.add("carousel__item--selected");
+
+            const selectedIndex = Array.from(items).indexOf(nextItem);
+
+            buttons.forEach((button) =>
+                button.classList.remove("carousel__button--selected")
+            );
+            buttons[selectedIndex].classList.add("carousel__button--selected");
+        } else {
+            // Reached the last item, send you to the first
+            selectedItem.classList.remove("carousel__item--selected");
+            items[0].classList.add("carousel__item--selected");
+
+            buttons.forEach((button) =>
+                button.classList.remove("carousel__button--selected")
+            );
+            buttons[0].classList.add("carousel__button--selected");
+        }
+    });
+    carousel.querySelector(".carousel__nav").appendChild(nextButton);
 });
